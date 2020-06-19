9019f2968958c1b7b3d35a8c811bff9eb95777d1
AdamWang00 <AdamWang00@users.noreply.github.com>
2020-06-19 03:14:37 -0400

    main

--- website/main/routes.py
+++ website/main/routes.py
@@ -8,12 +8,27 @@ main = Blueprint('main', __name__)
 @main.route("/")
 @main.route("/home")
 def home():
-	page = request.args.get('page', 1, type=int)
-	posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=10)
-	return render_template('home.html', posts=posts)
+	return render_template('home.html', title='Home')
 
 
 @main.route("/about")
 def about():
 	return render_template('about.html', title='About')
+
+
+@main.route("/events")
+def events():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=10)
+    return render_template('events.html', title='Upcoming Events', posts=posts)
+
+
+@main.route("/opportunities")
+def opportunities():
+    return render_template('opportunities.html', title='Opportunities Fall 2020')
+
+
+@main.route("/resources")
+def resources():
+    return render_template('resources.html', title='Resources')
 
--- website/posts/routes.py
+++ website/posts/routes.py
@@ -18,7 +18,7 @@ def new_post():
 		db.session.add(post)
 		db.session.commit()
 		flash('Your post has been created', 'success')
-		return redirect(url_for('main.home'))
+		return redirect(url_for('main.events'))
 	return render_template('create_post.html', title='New Post', form=form, legend="New Post")
 
 
@@ -56,5 +56,5 @@ def delete_post(post_id):
 	db.session.delete(post)
 	db.session.commit()
 	flash('Your post has been deleted', 'success')
-	return redirect(url_for('main.home'))
+	return redirect(url_for('main.events'))
 
