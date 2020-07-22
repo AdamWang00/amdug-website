99e323571fcbefe94cdc0d727f4cceccda32a3b9
AdamWang00 <AdamWang00@users.noreply.github.com>
2020-07-22 03:40:49 -0400

    move config to env

--- website/config.py
+++ website/config.py
@@ -1,15 +1,15 @@
 from os import environ
 
 class Config:
-	DEBUG = True #environ.get("DEBUG").lower() == "true" if "DEBUG" in environ else True
+	DEBUG = environ.get("DEBUG").lower() == "true" if "DEBUG" in environ else True
 	
 	SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
 	
 	MAIL_SERVER = 'smtp.googlemail.com'
 	MAIL_PORT = 587
 	MAIL_USE_TLS = True
-	MAIL_USERNAME = 'noreply10665@gmail.com' #os.environ.get('EMAIL_USER')
-	MAIL_PASSWORD = 'abc10665' #os.environ.get('EMAIL_PASS')
+	MAIL_USERNAME = environ.get('EMAIL_USER')
+	MAIL_PASSWORD = environ.get('EMAIL_PASS')
 
-	SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245' #os.environ.get('SECRET_KEY')
-	REGISTRATION_KEY = 'apma1650!' #os.environ.get('REGISTRATION_KEY')
+	SECRET_KEY = environ.get('SECRET_KEY')
+	REGISTRATION_KEY = environ.get('REGISTRATION_KEY')
